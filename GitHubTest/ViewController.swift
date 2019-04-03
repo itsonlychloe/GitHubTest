3351d660b4e6cfa8360f5eea7894188809f4dd25
Chloe Costello (s5125936) <s5125936@mac42949.student.bournemouth.ac.uk>
2019-04-03 11:34:18 +0100

    Removed comments

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  GitHubTest
-//
-//  Created by Chloe Costello (s5125936) on 03/04/2019.
-//  Copyright © 2019 Chloe Costello (s5125936). All rights reserved.
-//
 
 import UIKit
 
@@ -12,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
